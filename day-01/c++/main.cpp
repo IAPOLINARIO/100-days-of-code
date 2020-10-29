d13c1b92cc0c4724ede8c41b94a6226989ac012a
Alexandre Fatayer Canova <canova@lnxcit016642.cit>
2020-10-28 21:27:13 -0300

    Day 05 c++ version and dependencies refactoring for all previous challenges

--- day-01/c++/main.cpp
+++ day-01/c++/main.cpp
@@ -4,7 +4,7 @@
  * Just run 'make' to compile and run all the tests
  */
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN 
-#include "doctest.h" //https://github.com/onqtam/doctest
+#include "../../dependencies/c++/doctest.h" //https://github.com/onqtam/doctest
 
 int calcAge(int years) {
     return years * 365;
