255359f3e7e7559ee7a3193535ab0365b809aff3
Ismael Apolinario <ismaelapolinario@hotmail.com>
2020-10-30 17:08:47 +0100

    Adding tests

---
+++ day-06/Go/main_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_1_Prime(t *testing.T) {
+	testInput := 1
+	expectedResult := 2
+
+	result, _ := NextPrime(testInput)
+
+	if result != expectedResult {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_2_Prime(t *testing.T) {
+	testInput := 2
+	expectedResult := 2
+
+	result, _ := NextPrime(testInput)
+
+	if result != expectedResult {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_5_Prime(t *testing.T) {
+	testInput := 5
+	expectedResult := 5
+
+	result, _ := NextPrime(testInput)
+
+	if result != expectedResult {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_480_Prime(t *testing.T) {
+	testInput := 480
+	expectedResult := 487
+
+	result, _ := NextPrime(testInput)
+
+	if result != expectedResult {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_788_Prime(t *testing.T) {
+	testInput := 788
+	expectedResult := 797
+
+	result, _ := NextPrime(testInput)
+
+	if result != expectedResult {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_984_Prime(t *testing.T) {
+	testInput := 984
+	expectedResult := 991
+
+	result, _ := NextPrime(testInput)
+
+	if result != expectedResult {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
+
+func Test_997_Prime(t *testing.T) {
+	testInput := 997
+	expectedResult := 997
+
+	result, _ := NextPrime(testInput)
+
+	if result != expectedResult {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
