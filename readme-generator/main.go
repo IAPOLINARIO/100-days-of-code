394421d05cdf62bdd5790c587f65437db1b78525
Ismael Apolinario <ismaelapolinario@hotmail.com>
2020-11-02 00:09:54 +0100

    Adding more functions to the readme generator

--- readme-generator/main.go
+++ readme-generator/main.go
@@ -8,8 +8,12 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/IAPOLINARIO/100-days-of-code/readme-generator/structs"
+	"github.com/olekukonko/tablewriter"
 )
 
 const (
@@ -25,7 +29,9 @@ func main() {
 
 	//getCollaboratorStats(token)
 	//getCommits(token)
-	getPullRequests(token)
+	PRs := getPullRequests(token)
+
+	buildOutputResult(PRs)
 }
 
 func getGithubAPIResult(repoUrl string, token string) ([]byte, error) {
@@ -109,15 +115,98 @@ func getEvents(token string) {
 
 }
 
-func getPullRequests(token string) {
+func buildOutputResult(PRs []structs.PullRequest) {
+	req := make(map[string][]string)
+	for i := 0; i < len(PRs); i++ {
+		challengesDone := ""
+		totalPoints := 0
+		userHasDoneLabel := false
+
+		for _, l := range PRs[i].Labels {
+			if l.Name == "done" {
+				rg := regexp.MustCompile("day-\\w*")
+				match := rg.FindStringSubmatch(PRs[i].Title)
+
+				if len(match) > 0 {
+					userHasDoneLabel = true
+					if !strings.Contains(challengesDone, match[0]) {
+						challengesDone += match[0]
+						totalPoints += 100
+					}
+				}
+			}
+		}
+
+		if userHasDoneLabel {
+			if len(req[PRs[i].User.Login]) > 0 {
+				currentUserValue := req[PRs[i].User.Login]
+				challengesCombined := []string{currentUserValue[0], challengesDone}
+				currentTotalScore, _ := strconv.Atoi(currentUserValue[1])
+				newScore := totalPoints + currentTotalScore
+				req[PRs[i].User.Login] = []string{strings.Join(challengesCombined, ","), strconv.Itoa(newScore)}
+
+			} else {
+				req[PRs[i].User.Login] = []string{challengesDone, strconv.Itoa(totalPoints)}
+			}
+		}
+
+	}
+
+	sortedMap := SortMapByValue(req)
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Ranking", "Contributor", "Challenges", "Total Points"})
+
+	for k, v := range sortedMap {
+		result := []string{v[0], k, v[1], v[2]}
+		table.Append(result)
+	}
+
+	table.Render()
+}
+
+func SortMapByValue(Map map[string][]string) map[string][]string {
+
+	resultMap := make(map[string][]string)
+	// used to switch key and value
+	hack := map[float64][]string{}
+	hackkeys := []float64{}
+
+	for key, val := range Map {
+		NewKey, _ := strconv.ParseFloat(val[1], 64)
+		NewValue := []string{key, val[0]}
+
+		if len(hack[NewKey]) > 0 {
+			NewKey += 0.00001
+		}
+
+		hackkeys = append(hackkeys, NewKey)
+
+		hack[NewKey] = NewValue
+	}
+
+	sort.Slice(hackkeys, func(i, j int) bool {
+		return hackkeys[i] >= hackkeys[j]
+	})
+
+	for i := 0; i < len(hackkeys); i++ {
+		key := hack[hackkeys[i]][0]
+
+		score := fmt.Sprintf("%.0f", hackkeys[i])
+		value := []string{strconv.Itoa(i + 1), hack[hackkeys[i]][1], score}
+		resultMap[key] = value
+	}
+
+	return resultMap
+}
+
+func getPullRequests(token string) (PR []structs.PullRequest) {
 	bodyBytes, _ := getGithubAPIResult(baseGithubURI+pullRequestsURI, token)
 
+	fmt.Println(baseGithubURI + pullRequestsURI)
 	var PRs []structs.PullRequest
 	json.Unmarshal(bodyBytes, &PRs)
 
-	fmt.Printf("These are the pull requests:\n")
-	for _, p := range PRs {
-		fmt.Printf("PR: %v - Body: %v - Author: %v \n", p.Title, p.Body, p.User.Login)
-	}
-
+	return PRs
+	//fmt.Printf("These are the pull requests approved and done:\n")
 }
