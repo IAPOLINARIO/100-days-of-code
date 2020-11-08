9716fce1a9b9781f8d3e8753aab60ddc2aa7489a
Alexandre Fatayer Canova <canova@lnxcit016642.cit>
2020-11-08 19:20:54 -0300

    day-08 c++ version bugfix

--- Month-1/Week-2/day-08/c++/main.cpp
+++ Month-1/Week-2/day-08/c++/main.cpp
@@ -23,45 +23,46 @@ std::string getCharsFromDigit(char d) {
 }
 
 /**
- * Get all the letter combinations from a phone number.
- *
- * @param Phone Number
- * @return Vector with all possible letter combinations that the number could represent, ordered
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ * @param p Digits 
+ * @return string with all letter combinations
  */
-std::vector<std::string> letterCombinations(std::string p) {            
-    p.erase(std::remove_if(p.begin(), p.end(), checkInvalidOption), p.end()); //sanitize phone input, removing all non valid characters
-    
-    std::vector<std::string> r;
-    for (int w = 0; w < p.size(); w++) {            
-        std::string charsFromDigitStart = getCharsFromDigit(p[w]);        
-        for (int x = 0; x < charsFromDigitStart.size(); x++) {
-            if (p.size() == 1) {
-                r.push_back({charsFromDigitStart[x]});
-                continue;
-            }
-            for (int y = w + 1; y < p.size(); y++) {    
-                std::string charsFromDigitNext = getCharsFromDigit(p[y]);                
-                for (int z = 0; z < charsFromDigitNext.size(); z++) {                    
-                    r.push_back({charsFromDigitStart[x], charsFromDigitNext[z]});
-                }                
-            }            
+std::vector<std::string> letterCombinations(std::string p) {
+    p.erase(std::remove_if(p.begin(), p.end(), checkInvalidOption), p.end()); //sanitize phone input, removing all non valid characters    
+    std::vector<std::string> r{""};
+
+    for (int x = 0; x < p.size(); x++) {        
+        std::string charsFromDigit = getCharsFromDigit(p[x]);
+
+        std::vector<std::string> rAux;
+        for (int y = 0; y < charsFromDigit.size(); y++) {            
+            for (int z = 0; z < r.size(); z++) {                                
+                rAux.push_back(r[z] + charsFromDigit[y]);
+            }                      
         }        
-    }     
-    return r;   
+        r = rAux;   
+    }
+
+    std::sort(r.begin(), r.end()); 
+    if (r[0] == "") {
+        r.clear();
+    }
+
+    return r;
 }
 
 /**
  * Tests
  */
-TEST_CASE("Tests") {        
+TEST_CASE("Tests") {            
     CHECK(letterCombinations("") == std::vector<std::string>{});    
     CHECK(letterCombinations("1*A----01Z") == std::vector<std::string>{});    
     CHECK(letterCombinations("1*A----01Z2") == std::vector<std::string>{"a", "b", "c"}); 
     CHECK(letterCombinations("2") == std::vector<std::string>{"a", "b", "c"});        
-    CHECK(letterCombinations("23") == std::vector<std::string>{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"});            
     CHECK(letterCombinations("21") == std::vector<std::string>{"a", "b", "c"});    
+    CHECK(letterCombinations("23") == std::vector<std::string>{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"});            
     CHECK(letterCombinations("2-3") == std::vector<std::string>{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"});            
-    CHECK(letterCombinations("234") == std::vector<std::string>{"ad", "ae", "af", "ag", "ah", "ai", "bd", "be", "bf", "bg", "bh", "bi", "cd", "ce", "cf", "cg", "ch", "ci", "dg", "dh", "di", "eg", "eh", "ei", "fg", "fh", "fi"});
-    CHECK(letterCombinations("9") == std::vector<std::string>{"w", "x", "y", "z"});        
-    CHECK(letterCombinations("92") == std::vector<std::string>{"wa", "wb", "wc", "xa", "xb", "xc", "ya", "yb", "yc", "za", "zb", "zc"});
+    CHECK(letterCombinations("92") == std::vector<std::string>{"wa", "wb", "wc", "xa", "xb", "xc", "ya", "yb", "yc", "za", "zb", "zc"});  
+    CHECK(letterCombinations("456") == std::vector<std::string>{"gjm", "gjn", "gjo", "gkm", "gkn", "gko", "glm", "gln", "glo", "hjm", "hjn", "hjo", "hkm", "hkn", "hko", "hlm", "hln", "hlo", "ijm", "ijn", "ijo", "ikm", "ikn", "iko", "ilm", "iln", "ilo"});  
 }
