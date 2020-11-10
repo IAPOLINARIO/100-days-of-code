ebb541b6f7474dfd242e0679d851ec767a461182
Ismael Apolinario <ismaelapolinario@hotmail.com>
2020-11-10 23:35:49 +0100

    updating the ranking

--- Month-2/Week-5/day-30/Go/main.go
+++ Month-2/Week-5/day-30/Go/main.go
@@ -50,4 +50,15 @@ func Solution(N int) int {
 	return maxValue
 }
 
-//got 2658 expected 5268
+// Optmized solution found on internet
+// forming of the formula:
+// newVal = ((num / position) * (position * 10)) + (digit * position) + (num % position)
+
+// suppose, num = 276, starting from position = 1, and the digit = 3.
+
+// number will be in three parts: [left of 3] + [3] + [right of 3]
+
+// step 1: ((276/1) * (1*10)) + (3*1) + (276%1) = [2760]+[3]+[0] = [2763]
+// step 2: ((276/10) * (10*10)) + (3*10) + (276%10) = [2700]+[30]+[6] = [2736]
+// step 3: ((276/100) * (100*10)) + (3*100) + (276%100) = [2000]+[300]+[76] = [2376]
+// step 4: ((276/1000) * (1000*10)) + (3*1000) + (276%1000) = [0]+[3000]+[276] = [3276]
