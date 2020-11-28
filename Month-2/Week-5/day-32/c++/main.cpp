c15e5a87c3c751152d25ada2dfd3725aafcbbf5b
Alexandre Fatayer Canova <canova@lnxcit016642.cit>
2020-11-28 19:36:48 -0300

    day-32 c++ fix

--- Month-2/Week-5/day-32/c++/main.cpp
+++ Month-2/Week-5/day-32/c++/main.cpp
@@ -17,11 +17,21 @@
 std::vector<int> playerRank(std::vector<int> rankedScores, std::vector<int> playerScores) {    
     
     std::vector<int> playerRanking;
+    int playerGame = 0;
     for (auto &p : playerScores) {
         std::list<int> unifiedScoresAux;
         std::list<int> rankedScoresAux(rankedScores.begin(), rankedScores.end());
         unifiedScoresAux.merge(rankedScoresAux);
-        unifiedScoresAux.push_back(p);
+        
+        for (int i = 0; i < playerScores.size(); i++) {
+            if (i <= playerGame) {
+                unifiedScoresAux.push_back(playerScores[i]);                
+            } else {
+                break;
+            }
+        }
+        playerGame++;
+        
         unifiedScoresAux.sort();
         unifiedScoresAux.unique();
         
@@ -44,6 +54,6 @@ std::vector<int> playerRank(std::vector<int> rankedScores, std::vector<int> play
 TEST_CASE("Tests")
 {
     CHECK(playerRank({100, 90, 90, 80}, {70, 80, 105}) == std::vector<int>{4, 3, 1});
-    CHECK(playerRank({100, 90, 90, 80}, {106, 107, 105}) == std::vector<int>{1, 1, 1});
+    CHECK(playerRank({100, 90, 90, 80}, {106, 107, 105}) == std::vector<int>{1, 1, 3});
     CHECK(playerRank({100, 90, 90, 80}, {90, 80, 1}) == std::vector<int>{2, 3, 4});
 }
