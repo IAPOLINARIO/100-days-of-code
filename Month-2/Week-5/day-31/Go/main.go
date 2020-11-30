29ae2c27fcb6b1ccbfa9348a7964e1f26e6cfe78
Ismael Apolinario <ismaelapolinario@hotmail.com>
2020-11-30 23:56:31 +0100

    fixing day-31

--- Month-2/Week-5/day-31/Go/main.go
+++ Month-2/Week-5/day-31/Go/main.go
@@ -4,22 +4,6 @@ import (
 	"fmt"
 )
 
-var hourWord = map[int]string{
-	0:   "zero",
-	900: "CM",
-	500: "D",
-	400: "CD",
-	100: "C",
-	90:  "XC",
-	50:  "L",
-	40:  "XL",
-	10:  "X",
-	9:   "IX",
-	5:   "V",
-	4:   "IV",
-	1:   "I",
-}
-
 func main() {
 	result := timeInWords(5, 15)
 
