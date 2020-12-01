9871a21acbd987116877f5616e23da4867f737a1
Alexandre Fatayer Canova <canova@lnxcit016642.cit>
2020-11-30 21:15:37 -0300

    day-32 fix

--- Month-2/Week-5/day-32/c++/main.cpp
+++ Month-2/Week-5/day-32/c++/main.cpp
@@ -17,21 +17,24 @@
 std::vector<int> playerRank(std::vector<int> rankedScores, std::vector<int> playerScores) {    
     
     std::vector<int> playerRanking;
-    int playerGame = 0;
+    //int playerGame = 0; //leaderboard updated version of the challenge (disabled)
     for (auto &p : playerScores) {
         std::list<int> unifiedScoresAux;
         std::list<int> rankedScoresAux(rankedScores.begin(), rankedScores.end());
         unifiedScoresAux.merge(rankedScoresAux);
         
-        for (int i = 0; i < playerScores.size(); i++) {
+        //leaderboard updated version of the challenge (disabled)
+        /*for (int i = 0; i < playerScores.size(); i++) {
             if (i <= playerGame) {
                 unifiedScoresAux.push_back(playerScores[i]);                
             } else {
                 break;
             }
         }
-        playerGame++;
-        
+        playerGame++;*/
+        unifiedScoresAux.push_back(p); //comment this if the leaderboard updated version is active
+
+
         unifiedScoresAux.sort();
         unifiedScoresAux.unique();
         
@@ -54,6 +57,8 @@ std::vector<int> playerRank(std::vector<int> rankedScores, std::vector<int> play
 TEST_CASE("Tests")
 {
     CHECK(playerRank({100, 90, 90, 80}, {70, 80, 105}) == std::vector<int>{4, 3, 1});
-    CHECK(playerRank({100, 90, 90, 80}, {106, 107, 105}) == std::vector<int>{1, 1, 3});
+    CHECK(playerRank({100, 90, 90, 80}, {106, 107, 105}) == std::vector<int>{1, 1, 1});
     CHECK(playerRank({100, 90, 90, 80}, {90, 80, 1}) == std::vector<int>{2, 3, 4});
+    CHECK(playerRank({100, 90, 90, 80, 75, 60}, {50, 65, 77, 90, 102}) == std::vector<int>{6, 5, 4, 2, 1});
+    CHECK(playerRank({100, 100, 50, 40, 40, 20, 10}, {5, 25, 50, 120}) == std::vector<int>{6, 4, 2, 1});
 }
