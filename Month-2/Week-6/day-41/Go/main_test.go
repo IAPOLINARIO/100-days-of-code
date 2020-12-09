6ce08613248d8cc2ce3a7be6c0b94b2dfb820a5a
Ismael Apolinario <ismaelapolinario@hotmail.com>
2020-12-09 09:12:59 +0100

    Solving day-41

--- Month-2/Week-6/day-41/Go/main_test.go
+++ Month-2/Week-6/day-41/Go/main_test.go
@@ -57,3 +57,14 @@ func Test_4_Pairs(t *testing.T) {
 		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
 	}
 }
+
+func Test_5_Pairs(t *testing.T) {
+	input := []int{2, 7, 2, 1, 3, 8, 5, 2, 3, 5, 7, 8}
+	expectedResult := 5
+
+	result := sockPairs(input)
+
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
