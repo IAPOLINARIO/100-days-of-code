ba79aec81109abde743ad16ada37723f92f135ff
Ismael Apolinario <ismaelapolinario@hotmail.com>
2020-12-09 09:59:59 +0100

    Solving day-45

---
+++ Month-2/Week-7/day-45/Go/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(XOR(10, 41))
+}
+
+// function to swap two numbers  without
+// using temporary variable and returns the subtraction of them
+func XOR(x, y int) int {
+	x = x + y
+	y = x - y
+
+	return (x - y) - y
+}
