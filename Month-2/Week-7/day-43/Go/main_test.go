790f8b72e0b1932ff5c2b4cbf1ad564f146d2ffa
Ismael Apolinario <ismaelapolinario@hotmail.com>
2020-12-09 09:38:30 +0100

    Solving day-43

--- Month-2/Week-7/day-43/Go/main_test.go
+++ Month-2/Week-7/day-43/Go/main_test.go
@@ -40,3 +40,15 @@ func Test_Area_14(t *testing.T) {
 		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
 	}
 }
+
+func Test_Area_50(t *testing.T) {
+	base := 10
+	height := 10
+	expectedResult := 50
+
+	result := triArea(base, height)
+
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
