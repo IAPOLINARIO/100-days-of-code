0634de93f418a0b8090e73c205982b1eadc0c8cd
fzavalloni <fzavalloni@schubergphilis.com>
2020-12-28 21:26:12 +0100

    fzavalloni-day5-csharp

---
+++ Month-1/Week-01/day-05/csharp/SockPairs/SockPairs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SockPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sockets = "CABBACCC";
+
+            int socketPairs = Sockets.FindPairs(sockets);
+
+            Console.WriteLine($"Found [{socketPairs}] pairs");
+        }
+    }
+
+    public static class Sockets
+    {
+        public static int FindPairs(string sockets)
+        {
+            int socketPairs = 0;
+            var uniqueSocketList = new HashSet<char>(sockets);
+
+            foreach (char socket in uniqueSocketList)
+            {
+                socketPairs += CountPairs(socket, sockets);
+            }
+            return socketPairs / 2;
+        }
+
+        private static int CountPairs(char socket, string sockets)
+        {
+            return sockets.Count(item => item == socket);
+        }
+    }
+}
