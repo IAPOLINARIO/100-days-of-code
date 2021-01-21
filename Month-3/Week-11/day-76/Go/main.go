a93bf4c84cccd418a334fee4a36328f0bce5b8be
Ismael Apolinario <ismaelapolinario@hotmail.com>
2021-01-22 00:08:11 +0100

    Sorting function

--- Month-3/Week-11/day-76/Go/main.go
+++ Month-3/Week-11/day-76/Go/main.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Cake struct {
-	weight int
-	value  int
+	weight      int
+	value       int
+	costBenefit float32
+}
+
+func (c *Cake) GetCostBenefit() float32 {
+	if c.costBenefit == 0 {
+		c.costBenefit = float32(c.value) / float32(c.weight)
+	}
+
+	return c.costBenefit
+}
+
+type ByCostBenefit []Cake
+
+func (c ByCostBenefit) Len() int {
+	return len(c)
+}
+
+func (c ByCostBenefit) Less(i, j int) bool {
+	return c[i].costBenefit < c[j].costBenefit
+}
+
+func (c ByCostBenefit) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
 }
 
 func main() {
@@ -14,9 +40,23 @@ func main() {
 
 	result := maxDuffelBagValue(cakes, capacity)
 	fmt.Printf("Max capacity %d", result)
+
 }
 
 func maxDuffelBagValue(cakes []Cake, capacity int) int {
+	calcAndSortCostBenefit(&cakes)
+
+	for _, c := range cakes {
+		fmt.Printf("Cake cost benefit: %f \n", c.costBenefit)
+	}
 
 	return 0
+}
+
+func calcAndSortCostBenefit(cakes *[]Cake) {
+	for _, c := range *cakes {
+		c.GetCostBenefit()
+	}
+
+	sort.Sort(ByCostBenefit(*cakes))
 }
