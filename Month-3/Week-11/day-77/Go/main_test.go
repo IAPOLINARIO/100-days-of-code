8130ab89d52765c40fc82133af20c854997fd975
Ismael Apolinario <ismaelapolinario@hotmail.com>
2021-01-25 00:24:15 +0100

    Adding tests

---
+++ Month-3/Week-11/day-77/Go/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_Grid_One(t *testing.T) {
+	n := 5
+	r := 2
+	c := 3
+
+	grid := [][]string{{"-", "-", "-", "-", "-"}, {"-", "-", "-", "-", "-"}, {"p", "-", "-", "m", "-"}, {"-", "-", "-", "-", "-"}, {"-", "-", "-", "-", "-"}}
+
+	result := nextMove(n, r, c, grid)
+	expectedResult := "LEFT LEFT LEFT "
+
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Your evaluation was incorrect, got: %v, want: %v", result, expectedResult)
+	}
+}
