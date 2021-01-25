e013ee92e3d99dcbff195dda0418742b932ff8ed
Ismael Apolinario <ismaelapolinario@hotmail.com>
2021-01-25 18:34:02 +0100

    Building func

--- Month-3/Week-12/day-78/Go/main.go
+++ Month-3/Week-12/day-78/Go/main.go
@@ -5,3 +5,9 @@ import "fmt"
 func main() {
 	fmt.Print(buildNumber())
 }
+func buildNumber() string {
+	firstHalf := "...................."
+	secondeHalf := "---------------------"
+
+	return fmt.Sprintf("%v%v", len(firstHalf), len(secondeHalf))
+}
