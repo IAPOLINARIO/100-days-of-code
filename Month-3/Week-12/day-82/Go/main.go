04932456bfb12764e3144a779a0501be755b643d
Ismael Apolinario <ismaelapolinario@hotmail.com>
2021-02-01 22:53:30 +0100

    Adding non-decreasing func

--- Month-3/Week-12/day-82/Go/main.go
+++ Month-3/Week-12/day-82/Go/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+func isNonDecreasing(number int64) bool {
+	remainder := int64(10)
 
+	for number > 0 {
+		if number%10 <= remainder {
+			remainder = number % 10
+			number /= 10
+		} else {
+			return false
+		}
+	}
+
+	return true
 }
