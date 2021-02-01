80a4ea28efa6d8f91851a845cb5cc2a2d2e1fbb6
Ismael Apolinario <ismaelapolinario@hotmail.com>
2021-02-01 22:53:44 +0100

    Refactoring main func

--- Month-3/Week-12/day-82/Go/main.go
+++ Month-3/Week-12/day-82/Go/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func isNonDecreasing(number int64) bool {
 	remainder := int64(10)
 
@@ -13,4 +15,17 @@ func isNonDecreasing(number int64) bool {
 	}
 
 	return true
+}
+
+func main() {
+	input := []int64{4, 132, 1000, 7, 111111111111111110}
+
+	for i := int64(1); i <= input[0]; i++ {
+		if isNonDecreasing(input[i]) {
+			fmt.Printf("Case #%d: %d \n", i, input[i])
+		} else {
+			fmt.Printf("Case #%d: %d \n", i, lastTidyNumber(input[i]))
+		}
+	}
+
 }
