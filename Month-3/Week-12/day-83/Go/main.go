f5d00c5fb5d403b75f2bda80c66f029a30fcbae9
Ismael Apolinario <ismaelapolinario@hotmail.com>
2021-02-05 17:38:19 +0100

    Commit

--- Month-3/Week-12/day-83/Go/main.go
+++ Month-3/Week-12/day-83/Go/main.go
@@ -1,5 +1,39 @@
 package main
 
+type Stall struct {
+	n int
+	k int
+}
+
 func main() {
 
+}
+
+func checkStalls(input int, stalls []Stall){
+
+ for i := 1; i < input; i ++ {
+
+ }
+
+ 	spaces := make(map[int]int)
+	//for(int tc = 1; i <= t; tc++) {
+		//ll n, k; scanf("%lld %lld\n", &n, &k);
+		spaces.clear();
+		spaces[n]++;
+	
+		//ll ls = -1, rs = -1;
+		while(k > 0) {
+		  auto it = prev(spaces.end());
+		  ll size = it->first, count = it->second;
+		  spaces.erase(it);
+	
+		  ls = (size - 1) / 2;
+		  rs = size / 2;
+		  spaces[ls] += count;
+		  spaces[rs] += count;
+		  k -= count;
+		}
+		printf("Case #%d: %lld %lld\n", tc, max(ls, rs), min(ls, rs));
+	  }
+	  return 0;
 }
